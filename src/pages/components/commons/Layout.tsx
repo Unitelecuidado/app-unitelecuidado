a21d4c46c88bd154327db776ff595e957fdf7193
Julia Fazenda Ruiz <109627889+jufazenda@users.noreply.github.com>
2023-10-09 21:10:12 -0300

    inicio da tela de login, com inputs, faltando botão e alinhar tela

--- src/pages/components/commons/Layout.tsx
+++ src/pages/components/commons/Layout.tsx
@@ -2,15 +2,56 @@
 import { ReactNode } from 'react'
 import SideBar from './SideBar'
 
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { ThemeProvider, createTheme } from '@mui/material'
+
 interface LayoutProps {
   children: ReactNode
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#184066',
+    },
+  },
+})
+
 const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter()
+  const [isLogin, setIsLogin] = useState(false)
+
+  useEffect(() => { // pra alterar as cores de acordo com a tela
+    const updateBodyClass = () => {
+      if (router.pathname === '/') {
+        document.body.className = 'bg-padrao-green-light'
+        setIsLogin(true)
+      } else {
+        document.body.className = 'bg-padrao-gray'
+      }
+    }
+
+    updateBodyClass()
+
+    router.events.on('routeChangeComplete', updateBodyClass)
+
+    return () => {
+      router.events.off('routeChangeComplete', updateBodyClass)
+    }
+  }, [router.events, router.pathname])
+
   return (
-    <div className='font-gotham flex h-full m-4'>
-      <SideBar />
-      {children}
-    </div>
+    <ThemeProvider theme={theme}>
+      {isLogin ? (
+        <div className='font-gotham flex h-full m-4'>{children}</div>
+      ) : (
+        <div className='font-gotham flex h-full m-4'>
+          <SideBar />
+          {children}
+        </div>
+      )}
+    </ThemeProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,91 @@
+import { TextField, styled } from '@mui/material'
 import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
 
-const Home = () => {
+const PadraoTextField = styled(TextField)`
+  input {
+    color: #184066 !important; /* Defina a cor do texto */
+  }
+
+  .MuiOutlinedInput-root {
+    fieldset {
+      border: none;
+      color: #184066;
+    }
+    &:hover fieldset {
+      border: none;
+    }
+    &.Mui-focused fieldset {
+      border: none;
+    }
+  }
+`
+
+const Login = () => {
+  const [usuario, setUsuario] = useState('')
   return (
     <>
       <Head>
         <title>Unitelecuidado</title>
       </Head>
-      <main>
-       
-      </main>
+      <div className='w-full h-96vh flex justify-between mx-52 items-center'>
+        <div className='w-1/2 flex justify-center'>
+          <Image
+            src='/logos/logoVertical.png'
+            alt='Icone'
+            width={300}
+            height={300}
+            priority
+          />
+        </div>
+        <div className='w-1/2 flex bg-padrao-green rounded-md justify-center p-10'>
+          <div className='flex flex-col gap-5 w-2/3'>
+            <div className='bg-white rounded-md shadow-md'>
+              <PadraoTextField
+                type='String'
+                onChange={event => {
+                  setUsuario(event.target.value)
+                }}
+                value={usuario}
+                placeholder='Usuário'
+                sx={{
+                  '.MuiFormLabel-root': {
+                    alignItems: 'center',
+                    display: 'flex',
+                    height: '25px',
+                    color: '#184066',
+                    fontWeight: 500,
+                  },
+                  width: '100%',
+                }}
+              />
+            </div>
+            <div className='bg-white rounded-md shadow-md'>
+              <PadraoTextField
+                type='String'
+                onChange={event => {
+                  setUsuario(event.target.value)
+                }}
+                value={usuario}
+                placeholder='Senha'
+                sx={{
+                  '.MuiFormLabel-root': {
+                    alignItems: 'center',
+                    display: 'flex',
+                    height: '25px',
+                    color: '#184066',
+                    fontWeight: 500,
+                  },
+                  width: '100%',
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
 
-export default Home
+export default Login
